83e489a8299b675fdbed38432f35283090ece6a6
marc7274 <bonoms24@gmail.com>
2023-04-29 10:51:46 +0200

    Server online

--- server/server.py
+++ server/server.py
@@ -1,27 +1,55 @@
+#Versione aplha 0.0
 import json
 import socket
 import threading
 
-threads = list()
+class threadList():
+    def __init__(self):
+        self._list = list()
+        self._lock = threading.Lock()
 
-def thread_func(conn,addr):
+    def append(self, value):
+        with self._lock:
+            self._list.append(value)
+
+    def extend(self, values: list):
+        with self._lock:
+            self._list.extend(values)
+
+    def getList(self):
+        return self._list
+
+
+def thread_func(conn:socket.socket, addr, num_min, num_max):
     with conn:
-        recv = json.loads(conn.recv(1024))
-    return recv
-
-if __name__ == '__main__':
-    config = json.load(open())
-    
-    
-    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-        s.bind((config['ip'],config['port']))
-        s.listen()
+        conn.sendall(json.dumps({"min":num_min,"num_max":num_max}))
+        recv = conn.recv(1024)
+    prime_nums.extend(json.loads(recv))
         
-        while True:
-            num_min = int(input("numero da cui iniziare la ricerca: "))
-            num_max = int(input("ultimo numero della ricerca: "))
+
+threads = list()
+prime_nums = threadList()        
+                
+if __name__ == '__main__':
+    last_number_given = 0
+    config = json.load(open())
+
+    num_min = int(input("primo numero da calcolare: "))
+    num_max = int(input("ultimo numero da calcolare: "))
+    last_number_given = num_min - 1
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((config['ip'], config['port']))
+        s.listen()
+
+        while last_number_given<num_max:
+            conn,addr = s.accept()
+            if num_max-last_number_given>=config['range']:
+                threads.append(threading.Thread(target=thread_func,args=(conn,addr,last_number_given+1,last_number_given+config['range'])))
+            else:
+                threads.append(threading.Thread(target=thread_func,args=(conn,addr,last_number_given+1,last_number_given+(num_max-last_number_given))))
+            last_number_given+=num_max-last_number_given
+        for t in threads:
+            t.join()
             
-            while True:
-                conn, addr = s.accept()
-                thread = threading.Thread(target=thread_func, args=(conn,addr))
-                thread.start()
+        json.dump(json.loads(json.dumps(prime_nums.getList,open(config['path-to-output']+"result.json","w"))))
